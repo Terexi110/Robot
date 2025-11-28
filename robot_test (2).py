1e79c26bc8212057f840ca13df1b1d632e3712dd
nyte <oaoa.ovoaov@mail.ru>
2025-11-28 21:26:58 +0500

    Add files via upload
    ФИНАЛЬНЫЙ ФАЙЛ ДЛЯ РЕЗЕРВА

---
+++ robot_test (2).py
@@ -0,0 +1,125 @@
+import sys
+import time
+import random
+import math
+import os
+import threading
+
+
+class Program():
+    interpretation_started_timestamp = time.time() * 1000
+
+    pi = 3.141592653589793
+    gamepadpadButton1 = None
+    x = None
+    y = None
+
+    def kolesa(self, power_factor):
+
+        if (bool(gamepad.isPadPressed(1))):
+
+            # получаем значения с геймпада
+            self.x = [gamepad.padX(1), gamepad.padY(1)][0]
+            self.y = [gamepad.padX(1), gamepad.padY(1)][1]
+
+            if (self.x != 0 and self.y == 0):
+                # лево/право
+                brick.motor("M2").setPower(self.x * power_factor)
+                brick.motor("M1").setPower(-self.x * power_factor)
+            elif (self.y != 0):
+                # врепёд/назад
+                brick.motor("M1").setPower(self.y * power_factor)
+                brick.motor("M2").setPower(self.y * power_factor)
+
+        else:
+            brick.motor("M1").powerOff()
+            brick.motor("M2").powerOff()
+
+        script.wait(1)
+
+        return
+
+    def manipulyator(self, ):
+        if (bool(gamepad.isPadPressed(2))):
+            # получаем значения с геймпада
+            self.x = [gamepad.padX(2), gamepad.padY(2)][0]
+            self.y = [gamepad.padX(2), gamepad.padY(2)][1]
+            
+            if self.x == 100:
+                brick.motor("S1").setPower(-100)
+            elif self.x == -100:
+                brick.motor("S1").setPower(140)
+            if self.y == 100: 
+                brick.motor("S2").setPower(-90)
+            elif self.y == -100:
+                brick.motor("S2").setPower(10)
+                
+        else:
+            brick.motor("S2").powerOff()
+            brick.motor("S1").powerOff()
+
+        script.wait(10)
+
+        return
+
+    def control_motor_S1(self, x):
+        # Управляем сервоприводом
+        brick.motor("S2").setPower(x)
+
+    def execMain(self):
+        # запускаем видео-трансляцию
+        script.system(
+            "/etc/init.d/mjpg-encoder-ov7670 start --jpeg-qual 80 --white-black false && /etc/init.d/mjpg-streamer-ov7670 start")
+        self.x = 0
+        self.y = 0
+        self.flag = 1
+
+        self.lastx = 0
+        self.lasty = 0
+        power_factor = 1.0
+        while not gamepad.isConnected():
+            script.wait(10)
+
+        while True:
+            # выключение робота
+            if (gamepad.buttonWasPressed(5)):
+                os.system("halt")
+                
+            if gamepad.buttonWasPressed(2):
+                power_factor = 0.6
+            elif gamepad.buttonWasPressed(3):
+                power_factor = 1.0            
+
+            # отключаем сервопривод, отвечающий за зажатие
+            if (gamepad.buttonWasPressed(3)):
+              brick.motor("S2").power0ff()
+            
+            # отключаем сервопривод, отвечающий за зажатие
+            if (gamepad.buttonWasPressed(4)):
+                brick.motor("S1").powerOff()
+
+            # получаем флаг для выбора режика
+            # если кнопка 1 нажата - режим манипулятора
+            if (gamepad.buttonWasPressed(1)):
+                self.flag = 1
+            else:
+                self.flag = 0
+
+            if (self.flag == 0):
+                self.kolesa(power_factor)
+            else:
+                self.lastx = 0
+                self.lasty = 0
+                self.manipulyator()
+
+            script.wait(1)
+
+
+def main():
+    program = Program()
+    program.execMain()
+
+
+if __name__ == '__main__':
+    main()
+
